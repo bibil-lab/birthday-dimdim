d8fe2caf6b2aebe314c48143e005a2d60844592f
bilbilyaa <nbilaa2311@gmail.com>
2026-09-23 09:34:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,70 +13,70 @@ const pages = [
     {
         type: "photo",
         number: 1,
-        image: "foto1.jpg",
+        image: "images/foto1.jpg",
         text: "pertama kali bibil ngajak ngajak dimdim ngerjain tugas di Socrates Vegan"
     },
 
     {
         type: "photo",
         number: 2,
-        image: "foto2.jpg",
+        image: "images/foto2.jpg",
         text: "pertama kali kita nontonnn pulang kuliahh terus pulang nonton kitaa mamm"
     },
 
     {
         type: "photo",
         number: 3,
-        image: "foto3.jpg",
+        image: "images/foto3.jpg",
         text: "bibil ke rumahh dimdim krna mau ke pantee terus dari situu kita makinn dekett"
     },
 
     {
         type: "photo",
         number: 4,
-        image: "foto4.jpg",
+        image: "images/foto4.jpg",
         text: "kitaa ke pet cafee dan disituu bibil ceritainn kehidupann bibil ke dimdim krna bibil uda percyaa sma dimdim padahl kita belumm pcrann"
     },
 
     {
         type: "photo",
         number: 5,
-        image: "foto5.jpg",
+        image: "images/foto5.jpg",
         text: "kitaa uda pcarann dann ituu pertama kali kita pigii jauhhh"
     },
 
     {
         type: "photo",
         number: 6,
-        image: "foto6.jpg",
+        image: "images/foto6.jpg",
         text: "pertama kalinyaa bibil taunn baruan sma cowoo"
     },
 
     {
         type: "video",
         number: 7,
-        video: "video1.mp4",
+        video: "videos/video1.mp4",
         text: "kitaa mainn PlayStation, nntonn netflixx dann ituu masi maluu\" krna masi baruu\" pcalann, kalo sekarang sudahh tidaa ada maloe nyaaa"
     },
 
     {
         type: "video",
         number: 8,
-        video: "video2.mp4",
+        video: "videos/video2.mp4",
         text: "bibil screen recording dimdim lagi main gitar pass kitaa pecee, sekarang kita uda jarang pece hampir ga pernh punn :((("
     },
 
     {
         type: "video",
         number: 9,
-        video: "video3.mp4",
+        video: "videos/video3.mp4",
         text: "kitaa ke tamannn dann pulangnyaa kita nontonnn, ituu pass ulang tahun bibill, makacii yahh bibil sngtt senangg cekalii pada saat ituu"
     },
 
     {
         type: "video",
         number: 10,
-        video: "video4.mp4",
+        video: "videos/video4.mp4",
         text: "bibil ajak dimdim mainn tiktokk dann dimdimm mauuu, tapi sekarang dimdim gamauu lagi bibil ajak tiktokann :(((("
     }
 
@@ -695,8 +695,13 @@ function burstHearts(container, count = 14) {
 
 /* ==================================================
 INTRO TEXT ANIMATION
-"pibedeee dimdimmm" muncul huruf demi huruf
-di layar paling awal, sebelum situs dibuka
+"Happy Birthday Dimdim" muncul huruf demi huruf
+di layar paling awal, sebelum situs dibuka.
+
+Setiap KATA dibungkus dalam satu span ".word" (tidak
+boleh terpotong di tengah kata saat wrap ke baris baru),
+supaya teks tetap rapi, simetris, dan tetap di tengah
+tanpa menabrak / menutupi foto-foto di sekitarnya.
 ================================================== */
 
 function initIntroScreen() {
@@ -711,7 +716,7 @@ function initIntroScreen() {
         return;
     }
 
-    const message = "pibedeee dimdimmm";
+    const message = "Happy Birthday Dimdim";
 
     const colors = [
         "#95606d",
@@ -720,28 +725,68 @@ function initIntroScreen() {
         "#704650"
     ];
 
-    message.split("").forEach(function(char, index) {
+    const words = message.split(" ");
 
-        const letter =
+    let globalIndex = 0;
+
+    words.forEach(function(word, wordIndex) {
+
+        const wordEl =
             document.createElement("span");
 
-        letter.className =
-            "letter" + (char === " " ? " space" : "");
+        wordEl.className = "word";
 
-        letter.textContent =
-            char === " " ? "\u00A0" : char;
+        word.split("").forEach(function(char) {
 
-        letter.style.setProperty(
-            "--d",
-            (index * 0.07) + "s"
-        );
+            const letter =
+                document.createElement("span");
 
-        letter.style.setProperty(
-            "--c",
-            colors[index % colors.length]
-        );
+            letter.className = "letter";
 
-        textEl.appendChild(letter);
+            letter.textContent = char;
+
+            letter.style.setProperty(
+                "--d",
+                (globalIndex * 0.07) + "s"
+            );
+
+            letter.style.setProperty(
+                "--c",
+                colors[globalIndex % colors.length]
+            );
+
+            wordEl.appendChild(letter);
+
+            globalIndex++;
+
+        });
+
+        textEl.appendChild(wordEl);
+
+
+        /* Spasi di ANTARA kata dibuat sebagai elemen
+           terpisah supaya baris boleh membungkus di sini,
+           tapi tidak di tengah kata itu sendiri */
+
+        if (wordIndex < words.length - 1) {
+
+            const space =
+                document.createElement("span");
+
+            space.className = "letter space";
+
+            space.textContent = "\u00A0";
+
+            space.style.setProperty(
+                "--d",
+                (globalIndex * 0.07) + "s"
+            );
+
+            textEl.appendChild(space);
+
+            globalIndex++;
+
+        }
 
     });
 
